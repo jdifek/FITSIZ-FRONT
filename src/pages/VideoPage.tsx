f250b21c254731b42b5ba93ff31c6a17315e7094
jdifek <givotkov11@gmail.com>
2025-06-18 17:37:13 +0300

    wwqwq

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -17,15 +17,8 @@ const VideoPage: React.FC = () => {
   return (
     <div className="text-center mt-4">
       <h2 className="text-xl font-bold">Видео</h2>
-      {videos.length > 0 ? (
-        <ul className="mt-4 space-y-2">
-          {videos.map((video) => (
-            <li key={video.id}>{video.title}</li>
-          ))}
-        </ul>
-      ) : (
+      
         <p>Заглушка для раздела "Видео"</p>
-      )}
       <button
         className="mt-4 px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition duration-200"
         onClick={() => navigate('/welcome')}
